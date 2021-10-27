b91ba7b5c28a79a6ef638afe557b478b592df2ce
a-tsunoda <88137968+a-tsunoda@users.noreply.github.com>
2021-10-27 18:44:35 +0900

    1027/finish

--- src/index.js
+++ src/index.js
@@ -5,24 +5,60 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // li生成
   const li = document.createElement("li");
   li.className = "list-row";
 
   // p生成
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
   // button(完了)生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
     // 押された完了ボタンの親タグ(div)を未完了リストから削除し完了リストへ追加
-    // li.appendChild(p);
-    // li.appendChild()
 
     // 削除
     deleteFromIncompleteList(completeButton.parentNode);
+
+    // 完了リストへ追加する要素
+    const addTarget = completeButton.parentNode;
+    // 要素のテキストを取得
+    const text = addTarget.firstElementChild.innerText;
+    // div以下を取得
+    addTarget.textContent = null;
+    // li生成
+    const p = document.createElement("p");
+    p.innerText = text;
+    // button生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグを完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+      // テキストを取得
+      const text = backButton.parentNode.firstChild.innerText;
+      createIncompleteList(text);
+    });
+
+    // divタグの子要素に各要素を設定
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button(削除)生成
@@ -32,12 +68,6 @@ const onClickAdd = () => {
     // 押された削除ボタンの親タグ(div)を未完了リストから削除
     deleteFromIncompleteList(deleteButton.parentNode);
   });
-
-  // 未完了リストから指定の要素を削除
-  const deleteFromIncompleteList = (target) => {
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById("incomplete-list").removeChild(target);
-  };
 
   // divの子要素に書く要素を設定
   li.appendChild(p);
